f8c125ee453c5bcb99defeb04da1b3e7d6d4c085
Phan Lam <phanlam192@gmail.com>
2016-03-29 22:54:44 +0400

    MDL-20463 update : Write a new function for repeating element

--- lib/formslib.php
+++ lib/formslib.php
@@ -1125,6 +1125,113 @@ abstract class moodleform {
     }
 
     /**
+     * Method to add a repeating group of elements to a form.
+     *
+     * @param array $elementobjs Array of elements or groups of elements that are to be repeated
+     * @param int $repeats no of times to repeat elements initially
+     * @param array $options a nested array. The first array key is the element name.
+     *    the second array key is the type of option to set, and depend on that option,
+     *    the value takes different forms.
+     *         'default'    - default value to set. Can include '{no}' which is replaced by the repeat number.
+     *         'type'       - PARAM_* type.
+     *         'helpbutton' - array containing the helpbutton params.
+     *         'disabledif' - array containing the disabledIf() arguments after the element name.
+     *         'rule'       - array containing the addRule arguments after the element name.
+     *         'expanded'   - whether this section of the form should be expanded by default. (Name be a header element.)
+     *         'advanced'   - whether this element is hidden by 'Show more ...'.
+     * @param string $repeathiddenname name for hidden element storing no of repeats in this form
+     * @param string $addfieldsname name of button to apply
+     * @param string $repeatname name of box to fill in number of fields
+     * @param bool $addbuttoninside if true, don't call closeHeaderBefore($addfieldsname). Default false.
+     * @param string $type type of fields to repeat
+     * @return int no of repeats of element in this page
+     */
+    function repeat_elements2($elementobjs, $repeats, $options, $repeathiddenname, $addfieldsname, $addbuttoninside = false, $repeatname, $type) {
+        $repeats = optional_param($repeatname, $repeats, PARAM_INT);
+        if ($repeats <= 0) {
+            $repeats = optional_param($repeathiddenname, $repeats, PARAM_INT);
+        }
+        $mform =& $this->_form;
+        $mform->registerNoSubmitButton($addfieldsname);
+        $mform->addElement('hidden', $repeathiddenname, $repeats);
+        $mform->setType($repeathiddenname, PARAM_INT);
+        // Value not to be overridden by submitted value.
+        $mform->setConstants(array($repeathiddenname => $repeats));
+        $namecloned = array();
+        for ($i = 0; $i < $repeats; $i++) {
+            foreach ($elementobjs as $elementobj) {
+                $elementclone = fullclone($elementobj);
+                $this->repeat_elements_fix_clone($i, $elementclone, $namecloned);
+                if ($elementclone instanceof HTML_QuickForm_group && !$elementclone->_appendName) {
+                    foreach ($elementclone->getElements() as $el) {
+                        $this->repeat_elements_fix_clone($i, $el, $namecloned);
+                    }
+                    $elementclone->setLabel(str_replace('{no}', $i + 1, $elementclone->getLabel()));
+                }
+                $mform->addElement($elementclone);
+            }
+        }
+        for ($i = 0; $i < $repeats; $i++) {
+            foreach ($options as $elementname => $elementoptions) {
+                $pos = strpos($elementname, '[');
+                if ($pos !== false) {
+                    $realelementname = substr($elementname, 0, $pos)."[$i]";
+                    $realelementname .= substr($elementname, $pos);
+                } else {
+                    $realelementname = $elementname."[$i]";
+                }
+                foreach ($elementoptions as $option => $params) {
+                    switch ($option){
+                        case 'default' :
+                            $mform->setDefault($realelementname, str_replace('{no}', $i + 1, $params));
+                            break;
+                        case 'helpbutton' :
+                            $params = array_merge(array($realelementname), $params);
+                            call_user_func_array(array(&$mform, 'addHelpButton'), $params);
+                            break;
+                        case 'disabledif' :
+                            foreach ($namecloned as $num => $name) {
+                                if ($params[0] == $name) {
+                                    $params[0] = $params[0]."[$i]";
+                                    break;
+                                }
+                            }
+                            $params = array_merge(array($realelementname), $params);
+                            call_user_func_array(array(&$mform, 'disabledIf'), $params);
+                            break;
+                        case 'rule' :
+                            if (is_string($params)) {
+                                $params = array(null, $params, null, 'client');
+                            }
+                            $params = array_merge(array($realelementname), $params);
+                            call_user_func_array(array(&$mform, 'addRule'), $params);
+                            break;
+                        case 'type':
+                            $mform->setType($realelementname, $params);
+                            break;
+                        case 'expanded':
+                            $mform->setExpanded($realelementname, $params);
+                            break;
+                        case 'advanced' :
+                            $mform->setAdvanced($realelementname, $params);
+                            break;
+                    }
+                }
+            }
+        }
+        // Add form to input number of elements
+        $add = array();
+        $add[] =& $mform->createElement('text', $repeatname, get_string('numberofelements', 'form', $type), array('size' => '5', 'value' => $repeats));
+        $add[] =& $mform->createElement('submit', $addfieldsname, get_string('apply', 'form'));
+        $mform->addGroup($add, 'add', '', ' ', false);
+        if (!$addbuttoninside) {
+            $mform->closeHeaderBefore($addfieldsname);
+        }
+        return $repeats;
+    }
+
+
+    /**
      * Adds a link/button that controls the checked state of a group of checkboxes.
      *
      * @param int $groupid The id of the group of advcheckboxes this element controls
